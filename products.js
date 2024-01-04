1ce335cb0482e70f73f29544704ebd527597ac4a
Valetina-2608 <valusha2608@gmail.com>
2024-01-04 13:16:18 +0200

    Add product.js

---
+++ products.js
@@ -0,0 +1,56 @@
+let goods = [
+    {
+        "id":1,
+        "image":"img/apricot.jpg",
+        "caption":"Apricot",
+        "price":"3$",
+    },
+    {
+        "id":2,
+        "image":"img/carrot.jpg",
+        "caption":"Carrot",
+        "price":"2$",
+    },
+    {
+        "id":3,
+        "image":"img/feta_cheese.jpg",
+        "caption":"Feta cheese",
+        "price":"12$",
+    },
+    {
+        "id":4,
+        "image":"img/lettuce.jpg",
+        "caption":"Lettuce",
+        "price":"1$",
+    },
+    {
+        "id":5,
+        "image":"img/olive_oil.jpg",
+        "caption":"Olive oil",
+        "price":"7$",
+    },
+    {
+        "id":6,
+        "image":"img/apple.jpg",
+        "caption":"Apple",
+        "price":"2$",
+    },
+    {
+        "id":7,
+        "image":"img/dried_bannas.jpg",
+        "caption":"Dried bananas",
+        "price":"7$",
+    },
+    {
+        "id":8,
+        "image":"img/ice_cream.jpg",
+        "caption":"Ice cream",
+        "price":"7$",
+    },
+    {
+        "id":9,
+        "image":"img/beetroot.jpg",
+        "caption":"Beetroot",
+        "price":"7$",
+    }
+]
